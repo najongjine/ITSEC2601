31ce82d81f2c1334f2d02a3ab4e81649b9098205
najongjine <najongjin3@hotmail.com>
2026-01-07 13:19:35 +0900

    .

--- Day4.ts
+++ Day4.ts
@@ -1,3 +1,8 @@
+/** 소수(prime number) 판별기
+ * 1,2,3,5,7,11,13,15,17 이런게 소수에요
+ * @param 숫자
+ * @returns true | false.
+ */
 function isPrime(_num: number) {
   let bPrime = true;
   for (let i = 2; i < _num; i++) {
@@ -8,3 +13,6 @@ function isPrime(_num: number) {
   }
   return bPrime;
 }
+
+let result = isPrime(56567457);
+console.log(result);
