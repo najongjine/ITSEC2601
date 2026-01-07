f5c911fa228b1167a3b0f4bd61ebe7615abe606e
najongjine <najongjin3@hotmail.com>
2026-01-07 15:45:51 +0900

    .

--- Day4.ts
+++ Day4.ts
@@ -98,4 +98,12 @@ function randInt(min: number, max: number) {
 }
 
 /* for loop를 이용해서 mynums에 숫자 45개 채우세요 */
-let mynums = [];
+let mynums: number[] = [];
+for (; mynums.length <= 45; ) {
+  let randnum = randInt(1, 45);
+  if (mynums.find((e) => e == randnum)) {
+    continue;
+  }
+  mynums.push(randnum);
+}
+console.log(`mynums: `, mynums);
